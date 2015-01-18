4366ed07300ceb5bd0b7e295f1a1d8083155cbcf
Paolo Montano <Paolo.Montano@Gmail.com>
2015-01-17 23:28:24 -0800

    Updated template for index.

--- beachresort/application/views/index.php
+++ beachresort/application/views/index.php
@@ -1,44 +1,3 @@
-<!DOCTYPE html>
-<!-- Website template by freewebsitetemplates.com -->
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Bhaccasyoniztas Beach Resort Website Template</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-	<div id="background">
-		<div id="page">
-			<div id="header">
-				<div id="logo">
-					<a href="index.html"><img src="images/logo.png" alt="LOGO" height="112" width="118"></a>
-				</div>
-				<div id="navigation">
-					<ul>
-						<li class="selected">
-							<a href="index.html">Home</a>
-						</li>
-						<li>
-							<a href="about.html">About</a>
-						</li>
-						<li>
-							<a href="rooms.html">Rooms</a>
-						</li>
-						<li>
-							<a href="dives.html">Dive Site</a>
-						</li>
-						<li>
-							<a href="foods.html">Food</a>
-						</li>
-						<li>
-							<a href="news.html">News</a>
-						</li>
-						<li>
-							<a href="contact.html">Contact</a>
-						</li>
-					</ul>
-				</div>
-			</div>
 			<div id="contents">
 				<div id="adbox">
 					<img src="images/sea-sound.jpg" alt="Img">
@@ -94,40 +53,4 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<div id="footer">
-			<div>
-				<ul class="navigation">
-					<li class="active">
-						<a href="index.html">Home</a>
-					</li>
-					<li>
-						<a href="about.html">About</a>
-					</li>
-					<li>
-						<a href="rooms.html">Rooms</a>
-					</li>
-					<li>
-						<a href="dives.html">Dive Site</a>
-					</li>
-					<li>
-						<a href="foods.html">Food</a>
-					</li>
-					<li>
-						<a href="news.html">News</a>
-					</li>
-					<li>
-						<a href="contact.html">Contact</a>
-					</li>
-				</ul>
-				<div id="connect">
-					<a href="http://pinterest.com/fwtemplates/" target="_blank" class="pinterest"></a> <a href="http://freewebsitetemplates.com/go/facebook/" target="_blank" class="facebook"></a> <a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" class="twitter"></a> <a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" class="googleplus"></a>
-				</div>
-			</div>
-			<p>
-				© 2023 by BHACCASYONIZTAS BEACH RESORT. All Rights Reserved
-			</p>
-		</div>
 	</div>
-</body>
-</html>
